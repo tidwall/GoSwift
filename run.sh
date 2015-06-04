8920af12f9e05e5e508777a602691f1fdb39e6fa
Josh Baker <joshbaker77@gmail.com>
2015-06-04 08:12:38 -0700

    updated run.sh

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ f="$@"
 ext=${f##*.}
 
 if [[ "$ext" == "swift" ]]; then
-	cat go.swift > /tmp/go-main.swift
+	cat GoSwift/go.swift > /tmp/go-main.swift
 	echo "private var printmut = Mutex();func print(value: Any){printmut.lock {\"\\(value)\".writeToFile(\"/dev/stdout\", atomically:false, encoding:NSUTF8StringEncoding, error:nil)}}" >> /tmp/go-main.swift
 	echo "func println(value: Any){print(\"\\(value)\\n\")}" >> /tmp/go-main.swift
 	cat "$f" >> /tmp/go-main.swift
